807649b7653a0d248ee5bc134a429ef51ae404aa
zbtom <zbtom>
2016-12-27 16:02:01 +0100

    Clean up

--- test/CharacterCounterTest.java
+++ test/CharacterCounterTest.java
@@ -1,25 +1,27 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CharacterCounterTest {
 	@Test
 	public void count_stringA_returnOne() {
 		CharacterCounter counter = new CharacterCounter("A");
 		int countA = counter.count('A');
-		Assert.assertEquals(1, countA);
+		assertEquals(1, countA);
 	}
 
 	@Test
 	public void count_stringAA_returnTwo() {
 		CharacterCounter counter = new CharacterCounter("AA");
 		int countA = counter.count('A');
-		Assert.assertEquals(2, countA);
+		assertEquals(2, countA);
 	}
 
 	@Test
 	public void count_stringAA_returnsZeroForB(){
 		CharacterCounter counter = new CharacterCounter("AA");
 		int countB = counter.count('B');
-		Assert.assertEquals(0, countB);
+		assertEquals(0, countB);
 	}
 }
