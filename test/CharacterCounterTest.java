34e4bda5482fd41be48d347f6d55f8ab83ac6ea9
zbtom <zbtom>
2016-12-27 16:02:01 +0100

    Clean up

--- test/CharacterCounterTest.java
+++ test/CharacterCounterTest.java
@@ -2,22 +2,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CharacterCounterTest {
-	@org.junit.Test
-	public void counter_stringA_returnOne() throws Exception {
+	@Test
+	public void count_stringA_returnOne() {
 		CharacterCounter counter = new CharacterCounter("A");
 		int countA = counter.count('A');
 		Assert.assertEquals(1, countA);
 	}
 
 	@Test
-	public void count_stringAA_returnTwo() throws Exception {
+	public void count_stringAA_returnTwo() {
 		CharacterCounter counter = new CharacterCounter("AA");
 		int countA = counter.count('A');
 		Assert.assertEquals(2, countA);
 	}
 
 	@Test
-	public void count_stringAA_returnsZeroForB() throws Exception {
+	public void count_stringAA_returnsZeroForB(){
 		CharacterCounter counter = new CharacterCounter("AA");
 		int countB = counter.count('B');
 		Assert.assertEquals(0, countB);
